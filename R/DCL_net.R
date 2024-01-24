44222497e08a92e5695f9da02319a5b02c89687a
JH-42 <126781277+JH-42@users.noreply.github.com>
2024-01-23 19:59:10 -0600

    Update DCL_net.R

--- R/DCL_net.R
+++ R/DCL_net.R
@@ -41,11 +41,11 @@
 #'
 
 
-.onLoad <- function(libname, pkgname) {
-names(Mouse_Brain_core_marker)<-c("ependymal","micro_glial","oligodendrocyte","stromal_like_cell","endothelial","Schwann","glial_cell","neuron","astrocyte")
+#.onLoad <- function(libname, pkgname) {
+#names(Mouse_Brain_core_marker)<-c("ependymal","micro_glial","oligodendrocyte","stromal_like_cell","endothelial","Schwann","glial_cell","neuron","astrocyte")
 
-colnames(Mouse_Brain_labeling_matrix)<-c("astrocyte","endothelial","ependymal","stromal_like_cell","oligodendrocyte","micro_glial","glial_cell","neuron","Schwann")
-}
+#colnames(Mouse_Brain_labeling_matrix)<-c("astrocyte","endothelial","ependymal","stromal_like_cell","oligodendrocyte","micro_glial","glial_cell","neuron","Schwann")
+#}
 
 
 
