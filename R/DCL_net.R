67bc820d8ba3d9713578ef8900e4812dd4cac5fd
JH-42 <126781277+JH-42@users.noreply.github.com>
2024-02-13 22:43:33 -0600

    Update DCL_net.R

--- R/DCL_net.R
+++ R/DCL_net.R
@@ -41,31 +41,78 @@
 #'
 
 
-.onLoad <- function(libname, pkgname) {
-names(Mouse_Brain_core_marker)<-c("ependymal","micro_glial","oligodendrocyte","stromal_like_cell","endothelial","Schwann","glial_cell","neuron","astrocyte")
+DCL_net <- function(expression_data, geneList = NULL, tissueType = NULL, mult = FALSE, mult_tissue = NULL) {
+  combined_proportion <- list()
+  combined_marker_gene <- list()
+  # combined_Escore <- list()
+  # combined_potential_modules <- list()
+  
+  if (!mult) {
+    # single tissue
+    ep <- SSMD(bulk_data = expression_data, tissue = tissueType)
+    df <- as.matrix(do.call(cbind, ep$marker_gene))
 
-colnames(Mouse_Brain_labeling_matrix)<-c("astrocyte","endothelial","ependymal","stromal_like_cell","oligodendrocyte","micro_glial","glial_cell","neuron","Schwann")
-}
-
-
-
-DCL_net <-function(expression_data, geneList = NULL, tissueType,hub=1) {
-  if (tissueType == "Inflammatory") {
-    ep <- SSMD(bulk_data = expression_data, tissue = "Inflammatory")
-  }
-  if (tissueType == "Central Nervous System") {
-    ep <- SSMD(bulk_data = expression_data, tissue = "Central Nervous System")
-  }
-  if (tissueType == "Hematopoietic System") {
-    ep <- SSMD(bulk_data = expression_data, tissue = "Hematopoietic System")
-  }
-  if (tissueType == "Blood") {
-    ep <- SSMD(bulk_data = expression_data, tissue = "Blood")
+    {
+      gs <- melt(df, varnames = c("cellName", "geneID"), value.name = "expression")
+      gs <- gs[, c("geneID", "expression")]
+      colnames(gs) <- c("cellName", "geneID")
+      row.names(gs) <- gs$SYMBOL
+      symbol <- as.character(gs[, 2])
+      eg_gs <- bitr(symbol, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Mm.eg.db")
+      row.names(gs) <- gs$SYMBOL
+      row.names(eg_gs) <- eg_gs$SYMBOL
+      colnames(eg_gs) <- c("geneID", "ENTREZID")
+      
+      ggs <- merge(gs, eg_gs, by = "geneID")
+      ggs$geneID <- NULL
+      colnames(ggs) <- c("cellName", "geneID")
+      ggs <- ggs %>% distinct(cellName, geneID)
+    }
+    if (is.null(geneList)) {
+      return(list(marker = gs, cells_proportion = ep$Proportion))
+    } else {
+      symbol_2 <- as.character(geneList)
+      eg_gene <- bitr(symbol_2, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Mm.eg.db")
+      geneList <- bitr(geneList, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Mm.eg.db")
+      y <- clusterProfiler::enricher(gene = geneList$ENTREZID, TERM2GENE = ggs, minGSSize = 1, pvalueCutoff = 0.05, qvalueCutoff = 0.05, pAdjustMethod = "BH")
+    }
+    
+    y@keytype <- "ENTREZID"
+    y@organism <- "Mus musculus"
+    
+    n1 <- bitr(row.names(expression_data), fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Mm.eg.db")
+    n2 <- expression_data[n1$SYMBOL, ]
+    rownames(n2) <- n1$ENTREZID
+    
+    am <- bnpathplot(results = y, exp = n2, expRow = "ENTREZID", orgDb = org.Mm.eg.db, qvalueCutOff = 0.05, adjpCutOff = 0.05, R = 100, seed = 123,hub=1) # ,interactive=T, strengthPlot = T
+    
+    
+    # y <- clusterProfiler::enricher(eg_gene$ENTREZID, TERM2GENE=ggs, minGSSize=1,pvalueCutoff = 0.05,qvalueCutoff = 0.05,pAdjustMethod = "none")
+    
+    return(list(enricher = y, marker = gs, bnObject = am, cells_proportion = ep$Proportion, gene_scores = ep$Escore))
+    
+  } else {
+    # multi tissue
+    if (is.null(mult_tissue)) {
+      stop("mult_tissue cannot be NULL when mult is TRUE")
+    }
+    for (tissue in mult_tissue) {
+      ssmd_result <- SSMD(bulk_data = expression_data, tissue = tissue)
+      # 将每个组织的结果追加到对应的列表中
+      combined_proportion[[tissue]] <- ssmd_result$Proportion
+      combined_marker_gene[[tissue]] <- ssmd_result$marker_gene
+      combined_Escore[[tissue]] <- ssmd_result$Escore
+      # combined_potential_modules[[tissue]] <- ssmd_result$potential_modules
+    }
+  combined_prop_df <- do.call(rbind, combined_proportion)
+  combined_Escore_df <- do.call(rbind, combined_Escore)
+  ep <- list()
+  for (sublist in combined_marker_gene) {
+    ep <- c(final_list, as.list(sublist))
   }
   
-  
-  df <- as.matrix(do.call(cbind, ep$marker_gene))
-  
+  df <- as.matrix(do.call(cbind, ep))
+
   {
     gs <- melt(df, varnames = c("cellName", "geneID"), value.name = "expression")
     gs <- gs[, c("geneID", "expression")]
@@ -98,10 +145,11 @@ DCL_net <-function(expression_data, geneList = NULL, tissueType,hub=1) {
   n2 <- expression_data[n1$SYMBOL, ]
   rownames(n2) <- n1$ENTREZID
   
-  am <- bnpathplot(results = y, exp = n2, expRow = "ENTREZID", orgDb = org.Mm.eg.db, qvalueCutOff = 0.05, adjpCutOff = 0.05, R = 100, seed = 123,hub=hub) # ,interactive=T, strengthPlot = T
+  am <- bnpathplot(results = y, exp = n2, expRow = "ENTREZID", orgDb = org.Mm.eg.db, qvalueCutOff = 0.05, adjpCutOff = 0.05, R = 100, seed = 123,hub=1) # ,interactive=T, strengthPlot = T
   
   
   # y <- clusterProfiler::enricher(eg_gene$ENTREZID, TERM2GENE=ggs, minGSSize=1,pvalueCutoff = 0.05,qvalueCutoff = 0.05,pAdjustMethod = "none")
   
-  return(list(enricher = y, marker = gs, bnObject = am, cells_proportion = ep$Proportion, gene_scores = ep$Escore))
+  return(list(enricher = y, marker = gs, bnObject = am, cells_proportion = combined_prop_df, gene_scores = combined_Escore_df))
+  }
 }
